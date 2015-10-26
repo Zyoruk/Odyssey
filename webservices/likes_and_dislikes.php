6281e3ce3c80cd78e2da16c328d95eb56ba39a6c
zyoruk <luisangel1993julio@gmail.com>
2015-10-26 12:40:43 -0600

    Fixing syntax errors.

--- webservices/likes_and_dislikes.php
+++ webservices/likes_and_dislikes.php
@@ -7,10 +7,10 @@ if (isset ( $_POST['Like']) && isset($_POST['songid']) && isset ($_POST['uid']))
 	$songID = $_POST['songid'];
 	$userID = $_POST['uid'];
 
-	$sql = "INSERT Likes (song_id, user_id) VALUES('$songID' , '$userID' )";
+	$sql = "INSERT INTO Likes (song_id, user_id) VALUES('$songID' , '$userID' )";
 	if (!mysql_query($sql , $connection)){
 		$connection->close();
-		die("Error description: ", mysql_error($connection);
+		die("Error description: ". mysql_error($connection));
 	}
 }
 
@@ -19,13 +19,16 @@ else if(isset ( $_POST['Dislike']) && isset($_POST['songid']) && isset ($_POST['
 	$songID = $_POST['songid'];
 	$userID = $_POST['uid'];
 
-	$sql = "INSERT Dislikes (song_id, user_id) VALUES('$songID' , '$userID' )";
+	$sql = "INSERT INTO Dislikes (song_id, user_id) VALUES('$songID' , '$userID' )";
 	if (!mysql_query($sql , $connection)){
 		$connection->close();
-		die("Error description: ", mysql_error($connection);
+		die("Error description: ". mysql_error($connection));
 	}
+}else{
+	die ("Woops");
 }
+	
 
 $connection->close();
-
+exit();
 ?>
