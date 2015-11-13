344744d206effce12ee70cd87787fad7e3cf7294
zyoruk <luisangel1993julio@gmail.com>
2015-11-12 21:41:39 -0600

    udpated webservices used into html.

--- public_html/webservices/connect_sql.php
+++ public_html/webservices/connect_sql.php
@@ -18,7 +18,7 @@ mysql_select_db ($dbname, $conn );
 
 // Check connection
 if (! $conn) {
-	die ( "Connection failed: " . mysql_connect_error ());
+	die ( "{'error':Connection failed: " . mysql_connect_error ())."}";
 }
 
 ?>
